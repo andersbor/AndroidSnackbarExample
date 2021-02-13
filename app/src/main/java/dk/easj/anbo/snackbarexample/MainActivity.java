c5232563c539d9f542e23233e74db5a730a2e333
Anders Børjesson <anbo@zealand.dk>
2021-02-13 19:54:52 +0100

    Refactor: migrate to androix

--- app/src/main/java/dk/easj/anbo/snackbarexample/MainActivity.java
+++ app/src/main/java/dk/easj/anbo/snackbarexample/MainActivity.java
@@ -3,10 +3,11 @@ package dk.easj.anbo.snackbarexample;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private View layout;
@@ -27,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     public void mainButtonSnackbarAction(View view) {
         Snackbar snackbar = Snackbar
                 .make(layout, "Message is deleted", Snackbar.LENGTH_LONG)
-
                 .setAction("UNDO", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -46,14 +46,8 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View view) {
                     }
                 });
-
-// Changing message text color
         snackbar.setActionTextColor(Color.RED);
-
-// Changing action button text color
-        View sbView = snackbar.getView();
-        TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
-        textView.setTextColor(Color.YELLOW);
+        snackbar.setActionTextColor(Color.YELLOW);
         snackbar.show();
     }
 }
